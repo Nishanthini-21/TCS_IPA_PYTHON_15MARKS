014fb0f3e30c26d7b126d9ecc3a184d9f4e8fa37
Nishanthini-21 <140519126+Nishanthini-21@users.noreply.github.com>
2023-08-19 18:09:00 +0530

    Created question1.py

---
+++ question1.py
@@ -0,0 +1,39 @@
+'''Question1: Count the number of times, the given word is repeated in the user input strings(case insensitive)
+
+sample input and output:
+
+input:
+3
+HELLO ANMOL
+Hello friends!
+good morning
+hello
+
+output:
+count of given word:2'''
+
+
+def function(l,string1):
+    count=0
+    for v in l:
+        if v.lower()==string1.lower():
+            count+=1 
+    
+    return count
+
+
+l=[]
+for i in range(int(input())):
+    j=input().split(" ")
+    l=l+j
+print(l)
+str1=input()
+
+output=function(l,str1)
+if output==0:
+    print("word not found in input strings")
+else:
+    print(f"count of given word:{output}")
+
+
+
