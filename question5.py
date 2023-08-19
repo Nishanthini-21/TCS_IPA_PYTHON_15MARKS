68e9c1f125f9a7a7efc1ce51d372d606da136b76
Nishanthini-21 <140519126+Nishanthini-21@users.noreply.github.com>
2023-08-19 19:26:22 +0530

    Created question5.py

---
+++ question5.py
@@ -0,0 +1,28 @@
+'''
+Question5: Primenumber_count
+-----------------------------
+sample input and output:
+-----------------------------
+input:
+4
+3
+5
+7
+11
+
+output:
+4
+-----------------------------
+'''
+#solution
+from sympy import *
+l=[]
+count=0
+for i in range(int(input())):
+    l.append(int(input()))
+for i in l:
+    if isprime(i):
+        count+=1 
+    else:
+        pass
+print(count)
