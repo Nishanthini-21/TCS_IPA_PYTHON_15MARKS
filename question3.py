85bbe85b43cd89516966621274c58ec3c7935bd8
Nishanthini-21 <140519126+Nishanthini-21@users.noreply.github.com>
2023-08-19 18:42:35 +0530

    3rd question  is added

---
+++ question3.py
@@ -0,0 +1,26 @@
+'''Question3: To remove the vowels in the given string and also print the count of digits present in the string
+
+sample input and output:
+
+input:
+hello wordaeiou 123
+
+output:
+hll wrd 123 and count of digit is 3'''
+
+
+input1=input()
+digit_count=0
+l=[]
+for i in input1:
+    if i.isdigit():
+        digit_count+=1
+    if i in "aeiou":
+        pass
+    else:
+        l.append(i)
+
+s=""
+for i in l:
+    s+=i
+print(f"{s} and count of digit is {digit_count}")
